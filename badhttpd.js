84ff1d1e5270954cb8c7f028a524132955f5d208
Andrea Nall <anall@andreanall.com>
2013-12-09 22:50:49 -0700

    Actually 'parse' some http. still won't respond

--- badhttpd.js
+++ badhttpd.js
@@ -25,6 +25,11 @@ var argv= optimist
     .alias('timeout','t')
     .describe('timeout',"How long to hang, in seconds, 0 means forever")
 
+    // Disconnect
+    .boolean('disconnect')
+    .default('disconnect',false)
+    .describe('disconnect',"Disconnect instead of hanging")
+    
     // Misc stuff
     .boolean('help')
     // We're done
@@ -57,20 +62,63 @@ if ( argv.timeout > 0 ) {
 }
 
 // Client state machine
+var ST_INVALID = -2;
+var ST_HANG = -1;
 var ST_PRE = 0;
+var ST_READ_HEADER = 1;
+var ST_DONE_HEADER = 2;
 
-function gotLine(c,line) {
-    c._localData.lastTime = new Date().getTime();
-    var state = c._localData.state;
+var RX_METHOD_LINE = /^(GET|POST) (\/[^ ]*) (HTTP\/[0-9\.]+)$/;
+var RX_HEADER = /^([A-Za-z0-9-]+): (.+)$/;
+
+function gotLine(c,line,stream) {
+    var localData = c._localData;
+
+    localData.lastTime = new Date().getTime();
+    var state = localData.state;
     console.log(line);
-    c._localData.state = state;
+    if ( state == ST_PRE ) {
+        var data = line.match(RX_METHOD_LINE);
+        if ( data ) {
+            localData.method = data[1];
+            localData.uri = data[2];
+            localData.headers = {};
+            console.log(c._localData);
+            state = ST_READ_HEADER;
+        } else {
+            state = ST_INVALID;
+        }
+    } else if ( state == ST_READ_HEADER ) {
+        if ( line == "" ) {
+            state = ST_DONE_HEADER;
+        } else {
+            var data = line.match(RX_HEADER);
+            localData.headers[ data[1] ] = data[2];
+        }
+    } else if ( state == ST_DONE_HEADER ) {
+        state = ST_INVALID;
+    }
+
+    if ( state == ST_HANG ) {
+        if ( localData.disconnect ) {
+            c.end();
+        } else {
+            c.pause();
+        }
+    } else if ( state == ST_INVALID ) {
+        console.log("Reached invalid state");
+        c.end();
+    }
+
+    localData.state = state;
 }
 
 function onServerConnect(c) {
     clients.add( c );
     c._localData = {
         "lastTime": new Date().getTime(),
-        "state": ST_PRE
+        "state": ST_PRE,
+        "disconnect": argv.disconnect
     };
     c.setEncoding('utf8');
 
@@ -78,12 +126,17 @@ function onServerConnect(c) {
     c.on('end', function() {
         clients.remove( c );
         console.log('client disconnected: ', clients.size());
+        console.log( c._localData );
     });
 
     // If we don't want to read anything, never even start
     if ( argv.read ) {
         lineStream = byline.createStream( c, {"keepEmptyLines": true} );
-        lineStream.on('data',function(line) { gotLine(c,line); });
+        lineStream.on('data',function(line) { gotLine(c,line,this); });
+    } else if ( c._localData.disconnect ) {
+        c.end();
+    } else {
+        c.pause();
     }
 }
 
